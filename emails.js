555858f0ad4a2493f23a62c3918d6524dc9f5861
shellnumber007 <150329590+shellnumber007@users.noreply.github.com>
2023-11-09 04:03:33 -0400

    Add files via upload

---
+++ emails.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+// Read the JSON file
+fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading JSON file:', err);
+        return;
+    }
+
+    try {
+        const jsonData = JSON.parse(data);
+
+        // Extract email addresses using a regular expression
+        const emailRegex = /[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}/g;
+        const allEmails = JSON.stringify(jsonData).match(emailRegex) || [];
+
+        // Display the extracted email addresses
+        console.log('Extracted Email Addresses:');
+        allEmails.forEach((email) => {
+            console.log(email);
+        });
+    } catch (error) {
+        console.error('Error parsing JSON:', error);
+    }
+});
